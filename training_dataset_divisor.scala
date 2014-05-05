4b7d6585f490b30cf90a87ad7fc21f22623dc588
Sergio Álvarez <sergioalvarezsuarez@gmail.com>
2014-05-05 23:30:29 +0200

    Added logic for dividing the dataset according to the users, not the tweets

--- training_dataset_divisor.scala
+++ training_dataset_divisor.scala
@@ -3,26 +3,50 @@ exec scala "$0" "$@"
 !#
 import java.io._
 import scala.io.Source
+import scala.collection.mutable.ListBuffer
 
 val TRAINING_PERCENT = 0.8
 
 val dataset = args(0)
 val lines = Source.fromFile(dataset).getLines.toArray
-val forTraining = (lines.length * TRAINING_PERCENT).toInt
-val train = lines.slice(0, forTraining)
-val test  = lines.slice(forTraining, lines.length)
 
-writeLines(dataset + "_train", train)
-writeLines(dataset + "_test", test)
+val (trainingUsers, testingUsers) = divideUsersForDatasets(lines)
 
-def writeLines(datasetName:String, lines:Array[String]):Unit = {
+writeLines(dataset + "_train", trainingUsers, lines)
+writeLines(dataset + "_test", testingUsers, lines)
+
+/* ==================================================
+ *                   FUNCTIONS
+ * ================================================== */
+
+def divideUsersForDatasets(lines:Array[String]):(Array[String], Array[String]) = {
+  val users = new ListBuffer[String]
+  lines.foreach(line => {
+      val username = getUserByLine(line)
+      if(!users.contains(username)) users += username
+  })
+
+  val forTraining = (users.length * TRAINING_PERCENT).toInt
+  val train = users.slice(0, forTraining)
+  val test  = users.slice(forTraining, users.length)
+
+  (train.toArray, test.toArray)
+}
+
+def writeLines(datasetName:String, users:Array[String], lines:Array[String]):Unit = {
   val out = new File(datasetName)
   if(!out.exists) out.createNewFile
   val writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(out, true)))
   try {
-    lines.foreach(line => writer.write(line + "\n"))
+    lines.foreach(line => if(users.contains(getUserByLine(line))) writer.write(line + "\n"))
   } finally {
     writer.flush()
     writer.close()
   }
+}
+
+def getUserByLine(line:String):String = {
+  val features = line.split("\\|");
+  val fields = features(1).split(" ")
+  fields(1)
 }
